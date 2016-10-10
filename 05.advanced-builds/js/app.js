a1ff764b51c33701f824756fa97f3ab0bcf1d1cb
Phu Van Phan <phudev95@gmail.com>
2016-10-10 08:07:35 +0700

    Module 5: Organizing Files and Folders

---
+++ 05.advanced-builds/js/app.js
@@ -0,0 +1,6 @@
+require('./login');
+
+var content = document.getElementById("content");
+content.innerHTML = "<h1>App is here!!!</h1>";
+
+console.log('App loaded');
---
+++ 05.advanced-builds/webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+module.exports = {
+    // Set path folder of JS
+    context: path.resolve('js'),
+    entry: ['./utils.js', './app.js'],
+
+    output : {
+        path: path.resolve('build/js/'),
+        publicPath: '/public/assets/js/',
+        filename: "bundle-05.js"
+    },
+
+    // Set root folder is public (http://localhost:8080/webpack-dev-server/)
+    devServer: {
+        contentBase: 'public'
+    },
+
+    module: {
+        // Pre-loaders
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "jshint-loader"
+            }
+        ],
+
+        // Loaders
+        loaders: [
+            {
+                test: /\.es6$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.es6']
+    }
+};
